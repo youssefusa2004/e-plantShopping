dbf5dd58fa164dba7eab00e158f585e9004f76d5
youssefusa2004 <youssefusa43@gmail.com>
2025-09-04 08:44:44 -0400

    CartItems

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,44 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
-  // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach(item => {
+      const cost = parseFloat(item.cost.replace('$', ''));
+      total += cost * item.quantity;
+    });
+    return total.toFixed(2); 
+  };
+
  
-  };
-
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
-
-
+  
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.replace('$', ''));
+    const totalCost = cost * item.quantity;
+    return totalCost.toFixed(2);
   };
 
   return (
@@ -57,12 +72,10 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
