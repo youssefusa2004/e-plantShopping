f1f906776b9bf9e6ffdee0dfed58345e11ff0dd8
youssefusa2004 <youssefusa43@gmail.com>
2025-09-04 09:18:04 -0400

    Published

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './ProductList.css';
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux'; 
+import { useSelector, useDispatch } from 'react-redux'; 
 import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false);
-
-    const [addedToCart, setAddedToCart] = useState({});
     
+    const cartItems = useSelector(state => state.cart.items);
+    const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
 
     const plantsArray = [
@@ -94,30 +93,9 @@ function ProductList({ onHomeClick }) {
                     cost: "$22"
                 }
             ]
-        },
-        
+        }
     ];
-    const styleObj = {
-        backgroundColor: '#4CAF50',
-        color: '#fff!important',
-        padding: '15px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignIems: 'center',
-        fontSize: '20px',
-    }
-    const styleObjUl = {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '1100px',
-    }
-    const styleA = {
-        color: 'white',
-        fontSize: '30px',
-        textDecoration: 'none',
-    }
-
+    
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -127,36 +105,56 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(true); 
     };
-    const handlePlantsClick = (e) => {
-        e.preventDefault();
-        setShowPlants(true); 
-        setShowCart(false); 
-    };
 
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
     };
 
-    
     const handleAddToCart = (plant) => {
-        dispatch(addItem(plant)); 
-        
+        dispatch(addItem(plant));
         setAddedToCart((prevState) => ({
             ...prevState,
             [plant.name]: true,
         }));
     };
+    
+    const calculateTotalQuantity = () => {
+        return cartItems.reduce((total, item) => total + item.quantity, 0);
+    };
 
     return (
         <div>
-            <div className="navbar" style={styleObj}>
-
+            <div className="navbar">
+                <div className="tag">
+                     <div className="luxury">
+                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                        <a href="/" onClick={(e) => handleHomeClick(e)} className="tag_home_link">
+                            <div>
+                                <h3>Paradise Nursery</h3>
+                                <i>Where Green Meets Serenity</i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <ul className="ul">
+                    <div> <a href="#" onClick={() => setShowCart(false)}>Plants</a></div>
+                    <div className="cart_icon">
+                        <a href="#" onClick={handleCartClick}>
+                            <div className='cart'>
+                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                               
+                               <span className="cart_quantity_count">{calculateTotalQuantity()}</span>
+                            </div>
+                        </a>
+                    </div>
+                </ul>
             </div>
+            
             {!showCart ? (
                 <div className="product-grid">
                     
-                    {plantsArray.map((category, index) => (
+                     {plantsArray.map((category, index) => (
                         <div key={index}>
                             <div className='plantname_heading'>
                                 <h1 className='plant_heading'>{category.category}</h1>
@@ -173,11 +171,11 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">{plant.cost}</div>
                                         <button
-                                            className={`product-button ${addedToCart[plant.name] ? "added-to-cart" : ""}`}
+                                            className={`product-button ${addedToCart[plant.name] || (cartItems.find(item => item.name === plant.name)) ? "added-to-cart" : ""}`}
                                             onClick={() => handleAddToCart(plant)}
-                                            disabled={addedToCart[plant.name]}
+                                            disabled={addedToCart[plant.name] || (cartItems.find(item => item.name === plant.name))}
                                         >
-                                            {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
+                                            {cartItems.find(item => item.name === plant.name) ? "Added to Cart" : "Add to Cart"}
                                         </button>
                                     </div>
                                 ))}
